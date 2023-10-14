26c17cdf3e57ea1df82d0fcd8ccb5a012abbefb8
Jaivik Jariwala <112539487+Jaivik-Jariwala@users.noreply.github.com>
2023-10-14 12:46:47 +0530

    added : ReactApp

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Navbar/>
+      <About/>
+      <Project/>
+      <Skills/>
+      <Events/>
+      <Licences_Certifications/>
+      <resume_CV/>
+      <Contact/>
+    </main>
+  );
+}
+
+export default App;
